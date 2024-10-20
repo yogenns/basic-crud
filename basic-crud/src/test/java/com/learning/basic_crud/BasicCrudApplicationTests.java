83cf3ffb9090a3ba3a5a95294c350eb8574ed2c5
Yogenns <yogenns@gmail.com>
2024-10-20 19:46:57 +0530

    Initial Commit

---
+++ basic-crud/src/test/java/com/learning/basic_crud/BasicCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.basic_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
